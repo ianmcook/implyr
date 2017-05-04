aca1546ceedfcb3fbbb67ef8cfcf7968a15d75de
Ian Cook <ian@cloudera.com>
2017-05-04 09:43:32 -0400

    More changes for dplyr 0.6.0 compatibility

--- R/src_impala.R
+++ R/src_impala.R
@@ -94,12 +94,6 @@ pkg_env <- new.env()
 #' @importFrom DBI dbExecute
 #' @importFrom DBI dbGetInfo
 #' @importFrom DBI dbSendQuery
-#' @rawNamespace
-#' if (utils::packageVersion("dplyr") > "0.5.0") {
-#'   importFrom(dbplyr,src_sql)
-#' } else {
-#'   importFrom(dplyr,src_sql)
-#' }
 #' @importFrom methods callNextMethod
 #' @importFrom methods existsMethod
 #' @importFrom methods isClass
@@ -240,6 +234,7 @@ src_impala <- function(drv, ..., auto_disconnect = FALSE) {
   }, where = topenv(parent.frame()))
 
   con <- structure(con, class = c("impala_connection", class(con)))
+  attributes(con)$info <- info
 
   src_sql("impala",
           con = con,
@@ -258,7 +253,37 @@ src_impala <- function(drv, ..., auto_disconnect = FALSE) {
 #'   importFrom(dplyr,db_desc)
 #' }
 db_desc.impala_connection <- function(x) {
-  "TBD"
+  info <- attr(x, "info")
+  if(is.null(info)) {
+    return("???")
+  }
+  info$version <-
+    sub("\\s?.?(buil|release).*$", "", info$version, ignore.case = TRUE)
+  if (!"url" %in% names(info)) {
+    if (!is.null(info$host) && !is.null(info$port)) {
+      info$url <- paste0(info$host, ":", info$port)
+    } else if (!is.null(info$host)) {
+      info$url <- info$host
+    } else if (!is.null(info$dsn)) {
+      info$url <- paste0(info$dsn)
+    }
+  } else {
+    info$url <- paste0(info$url)
+  }
+  if (!is.null(info$user) && info$user != "") {
+    info$user <- paste0(info$user, "@")
+  }
+  paste0(
+    info$version,
+    " through ",
+    info$package,
+    " [",
+    info$user,
+    info$url,
+    "/",
+    info$dbname,
+    "]"
+  )
 }
 
 #' Describe the Impala data source
@@ -314,12 +339,6 @@ src_desc.src_impala <- function(x) {
 #' \dontrun{
 #' flights_tbl <- tbl(impala, "flights")}
 #' @export
-#' @rawNamespace
-#' if (utils::packageVersion("dplyr") > "0.5.0") {
-#'   importFrom(dbplyr,tbl_sql)
-#' } else {
-#'   importFrom(dplyr,tbl_sql)
-#' }
 #' @importFrom dplyr tbl
 tbl.src_impala <- function(src, from, ...) {
   tbl_sql("impala", src = src, from = from, ...)
@@ -338,31 +357,11 @@ sql_escape_string.impala_connection <- function(con, x) {
 }
 
 #' @export
-#' @rawNamespace
-#' if (utils::packageVersion("dplyr") > "0.5.0") {
-#'   importFrom(dbplyr,base_agg)
-#'   importFrom(dbplyr,base_scalar)
-#'   importFrom(dbplyr,base_win)
-#'   importFrom(dbplyr,build_sql)
-#'   importFrom(dbplyr,sql)
-#'   importFrom(dbplyr,sql_prefix)
-#'   importFrom(dbplyr,sql_translator)
-#'   importFrom(dbplyr,sql_variant)
-#' } else {
-#'   importFrom(dplyr,base_agg)
-#'   importFrom(dplyr,base_scalar)
-#'   importFrom(dplyr,base_win)
-#'   importFrom(dplyr,build_sql)
-#'   importFrom(dplyr,sql)
-#'   importFrom(dplyr,sql_prefix)
-#'   importFrom(dplyr,sql_translator)
-#'   importFrom(dplyr,sql_variant)
-#' }
 #' @importFrom dplyr sql_translate_env
 sql_translate_env.impala_connection <- function(con) {
   sql_variant(
     sql_translator(
-      .parent = base_scalar,
+      .parent = base_scalar(),
 
       # type conversion functions
       as.character = function(x)
@@ -467,7 +466,7 @@ sql_translate_env.impala_connection <- function(con) {
 
     ),
     sql_translator(
-      .parent = base_agg,
+      .parent = base_agg(),
       n = function()
         sql("count(*)"),
       median = sql_prefix("appx_median"),
@@ -480,7 +479,7 @@ sql_translate_env.impala_connection <- function(con) {
         build_sql("group_concat(", x, ",'')")
       }
     ),
-    base_win
+    base_win()
   )
 }
 
@@ -497,18 +496,6 @@ setdiff.tbl_impala <- function(x, y, copy = FALSE, ...) {
 }
 
 #' @export
-#' @rawNamespace
-#' if (utils::packageVersion("dplyr") > "0.5.0") {
-#'   importFrom(dbplyr,build_sql)
-#'   importFrom(dbplyr,ident)
-#'   importFrom(dbplyr,is.ident)
-#'   importFrom(dbplyr,sql)
-#' } else {
-#'   importFrom(dplyr,build_sql)
-#'   importFrom(dplyr,ident)
-#'   importFrom(dplyr,is.ident)
-#'   importFrom(dplyr,sql)
-#' }
 #' @importFrom dplyr sql_subquery
 #' @importFrom stats setNames
 #' @importFrom utils getFromNamespace
@@ -698,14 +685,6 @@ copy_to.src_impala <-
 #' @importFrom assertthat assert_that
 #' @importFrom assertthat is.string
 #' @importFrom assertthat is.flag
-#' @rawNamespace
-#' if (utils::packageVersion("dplyr") > "0.5.0") {
-#'   importFrom(dbplyr,sql_render)
-#'   importFrom(dbplyr,op_vars)
-#' } else {
-#'   importFrom(dplyr,sql_render)
-#'   importFrom(dplyr,op_vars)
-#' }
 #' @importFrom dplyr %>%
 #' @importFrom dplyr compute
 #' @importFrom dplyr group_by_
@@ -803,12 +782,6 @@ collapse.tbl_impala <- function(x, vars = NULL, ...) {
 #' @importFrom assertthat assert_that
 #' @importFrom assertthat is.string
 #' @importFrom assertthat is.flag
-#' @rawNamespace
-#' if (utils::packageVersion("dplyr") > "0.5.0") {
-#'   importFrom(dbplyr,ident)
-#' } else {
-#'   importFrom(dplyr,ident)
-#' }
 #' @importFrom dplyr db_save_query
 db_save_query.impala_connection <-
   function(con,
@@ -923,12 +896,6 @@ db_drop_table.impala_connection <-
 #' @importFrom assertthat assert_that
 #' @importFrom assertthat is.string
 #' @importFrom assertthat is.flag
-#' @rawNamespace
-#' if (utils::packageVersion("dplyr") > "0.5.0") {
-#'   importFrom(dbplyr,escape)
-#' } else {
-#'   importFrom(dplyr,escape)
-#' }
 #' @importFrom dplyr db_insert_into
 db_insert_into.impala_connection <-
   function(con, table, values, overwrite = FALSE, ...) {
@@ -987,16 +954,6 @@ db_data_type.impala_connection <- function(con, fields, ...) {
 #' @importFrom assertthat assert_that
 #' @importFrom assertthat is.string
 #' @importFrom assertthat is.flag
-#' @rawNamespace
-#' if (utils::packageVersion("dplyr") > "0.5.0") {
-#'   importFrom(dbplyr,escape)
-#'   importFrom(dbplyr,ident)
-#'   importFrom(dbplyr,sql_vector)
-#' } else {
-#'   importFrom(dplyr,escape)
-#'   importFrom(dplyr,ident)
-#'   importFrom(dplyr,sql_vector)
-#' }
 #' @importFrom dplyr db_create_table
 db_create_table.impala_connection <-
   function (con,
@@ -1226,3 +1183,165 @@ assertthat::on_failure(is_nchar_one_string_or_null) <-
   function(call, env) {
     paste0(deparse(call$x), " is not a string with one character")
   }
+
+# dplyr 0.5.0-0.6.0 compatibility
+base_agg <- function ()
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::base_agg
+  }
+  else {
+    dplyr::base_agg
+  }
+}
+base_scalar <- function ()
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::base_scalar
+  }
+  else {
+    dplyr::base_scalar
+  }
+}
+base_win <- function ()
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::base_win
+  }
+  else {
+    dplyr::base_win
+  }
+}
+build_sql <- function (...)
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::build_sql(...)
+  }
+  else {
+    dplyr::build_sql(...)
+  }
+}
+sql <- function (...)
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::sql(...)
+  }
+  else {
+    dplyr::sql(...)
+  }
+}
+sql_prefix <- function (...)
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::sql_prefix(...)
+  }
+  else {
+    dplyr::sql_prefix(...)
+  }
+}
+sql_translator <- function (...)
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::sql_translator(...)
+  }
+  else {
+    dplyr::sql_translator(...)
+  }
+}
+sql_variant <- function (...)
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::sql_variant(...)
+  }
+  else {
+    dplyr::sql_variant(...)
+  }
+}
+ident <- function (...)
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::ident(...)
+  }
+  else {
+    dplyr::ident(...)
+  }
+}
+is.ident <- function (...)
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::is.ident(...)
+  }
+  else {
+    dplyr::is.ident(...)
+  }
+}
+escape <- function (...)
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::escape(...)
+  }
+  else {
+    dplyr::escape(...)
+  }
+}
+sql_vector <- function (...)
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::sql_vector(...)
+  }
+  else {
+    dplyr::sql_vector(...)
+  }
+}
+sql_render <- function (...)
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::sql_render(...)
+  }
+  else {
+    dplyr::sql_render(...)
+  }
+}
+op_vars <- function (...)
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::op_vars(...)
+  }
+  else {
+    dplyr::op_vars(...)
+  }
+}
+src_sql <- function (...)
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::src_sql(...)
+  }
+  else {
+    dplyr::src_sql(...)
+  }
+}
+tbl_sql <- function (...)
+{
+  if (utils::packageVersion("dplyr") > "0.5.0") {
+    dplyr::check_dbplyr()
+    dbplyr::tbl_sql(...)
+  }
+  else {
+    dplyr::tbl_sql(...)
+  }
+}
