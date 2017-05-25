aaab3608a35486fff5ea6708e44dceb048f2ef28
Ian Cook <ian@cloudera.com>
2017-05-25 17:09:44 -0400

    Revert "Add test that fails to confirm tests are running on Travis"
    This reverts commit ed745364102d51afa86a7fd4e32430626c44021d.

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -7,8 +7,3 @@ test_that("tbl_impala matches tbl_df", {
   }
   compare_tbls(list(tbl(impala, "airlines"), nycflights13::airlines), op = test_op)
 })
-
-test_that("expect failure", {
-  test_op <- function(x) x
-  compare_tbls(list(iris, mtcars), op = test_op)
-})
