8ab404a921eeebc4715856cba42b2d6a65811495
Ian Cook <ian@cloudera.com>
2017-06-21 08:43:27 -0400

    Tests using Lahman::Batting

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -30,3 +30,28 @@ test_that("airlines tbl_impala matches airlines tbl_df", {
   }
   compare_tbls(list(tbl(impala, "airlines"), nycflights13::airlines), op = test_op)
 })
+
+test_that("batting table exists in lahman database", {
+  check_impala()
+  expect_true("batting" %in% dbGetQuery(impala, "SHOW TABLES IN lahman")$name)
+})
+
+test_that("batting tbl_impala has same column names as Batting tbl_df (ignoring case)", {
+  check_impala()
+  test_op <- function(x) {
+    x %>% colnames() %>% tolower()
+  }
+  expect_true(
+    compare(test_op(tbl(impala, in_schema("lahman", "batting"))), test_op(Lahman::Batting))$equal
+  )
+})
+
+test_that("batting tbl_impala has same number of rows as Batting tbl_df", {
+  check_impala()
+  test_op <- function(x) {
+    x %>% summarize(n()) %>% collect() %>% as.integer()
+  }
+  expect_true(
+    compare(test_op(tbl(impala, in_schema("lahman", "batting"))), test_op(Lahman::Batting))$equal
+  )
+})
