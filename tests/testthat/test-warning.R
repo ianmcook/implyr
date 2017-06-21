aff3743f7e15d200266672576b5e402770e5013b
Ian Cook <ian@cloudera.com>
2017-06-21 08:14:48 -0400

    Clean-up

--- tests/testthat/test-warning.R
+++ tests/testthat/test-warning.R
@@ -1,9 +1,8 @@
-context("warnings")
+context("expected warnings")
 
 test_that("warning when arrange() before other verbs", {
   check_impala()
   test_op <- function(x) {
-    # sql_build(x %>% arrange(year) %>% head(10) %>% arrange(day))
     x %>% arrange(year) %>% head(10) %>% arrange(day) %>% collect()
   }
   expect_warning(test_op(tbl(impala, "flights")))
