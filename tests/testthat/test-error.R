02cfb24916cebb8a04e0e823cd455c6f14d4c77b
Ian Cook <ian@cloudera.com>
2017-06-21 09:18:39 -0400

    Tests for expected errors on unsupported window functions

---
+++ tests/testthat/test-error.R
@@ -0,0 +1,56 @@
+context("expected errors")
+
+test_that("error when using unsupported window function median()", {
+  check_impala()
+  test_op <- function(x) {
+    x %>% group_by(playerid) %>% mutate(median(g)) %>% collect()
+  }
+  expect_error(
+    test_op(tbl(impala, in_schema("lahman", "batting"))),
+    regexp = "not supported"
+  )
+})
+
+test_that("error when using unsupported window function n_distinct()", {
+  check_impala()
+  test_op <- function(x) {
+    x %>% group_by(playerid) %>% mutate(n_distinct(g)) %>% collect()
+  }
+  expect_error(
+    test_op(tbl(impala, in_schema("lahman", "batting"))),
+    regexp = "not supported"
+  )
+})
+
+test_that("error when using unsupported window function ndv()", {
+  check_impala()
+  test_op <- function(x) {
+    x %>% group_by(playerid) %>% mutate(ndv(g)) %>% collect()
+  }
+  expect_error(
+    test_op(tbl(impala, in_schema("lahman", "batting"))),
+    regexp = "not supported"
+  )
+})
+
+test_that("error when using unsupported window function sd()", {
+  check_impala()
+  test_op <- function(x) {
+    x %>% group_by(playerid) %>% mutate(sd(g)) %>% collect()
+  }
+  expect_error(
+    test_op(tbl(impala, in_schema("lahman", "batting"))),
+    regexp = "not supported"
+  )
+})
+
+test_that("error when using unsupported window function var()", {
+  check_impala()
+  test_op <- function(x) {
+    x %>% group_by(playerid) %>% mutate(var(g)) %>% collect()
+  }
+  expect_error(
+    test_op(tbl(impala, in_schema("lahman", "batting"))),
+    regexp = "not supported"
+  )
+})
