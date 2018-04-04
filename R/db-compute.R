d418aceaa39a8436aef64f043eea697bc7d5db88
Ian Cook <ian@cloudera.com>
2018-04-04 11:57:54 -0400

    Bugfix
    Resolves: Error in x$from : $ operator is invalid for atomic vectors

--- R/db-compute.R
+++ R/db-compute.R
@@ -28,6 +28,9 @@ db_sql_render.impala_connection <- function(con, sql, ...) {
 }
 
 has_order_by_in_subquery <- function(x) {
+  if (inherits(x, "ident")) {
+    return(FALSE)
+  }
   if (!inherits(x$from, "select_query")) {
     return(FALSE)
   }
