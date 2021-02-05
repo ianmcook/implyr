2d207ede7efec095e068d8554395daee8fe6029c
Ian Cook <ianmcook@gmail.com>
2021-02-05 17:30:34 -0500

    Fix handling of field_terminator and line_terminator

--- R/src_impala.R
+++ R/src_impala.R
@@ -383,7 +383,7 @@ copy_to.src_impala <-
         external = external,
         force = force,
         field_terminator = field_terminator,
-        line_terminator = field_terminator,
+        line_terminator = line_terminator,
         file_format = file_format,
         ...
       )
