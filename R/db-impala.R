eb3874122c1e0d02544efd62ff5fdb9c088c0558
Ian Cook <ianmcook@gmail.com>
2021-03-29 09:56:44 -0400

    Register virtual S3 class impala_connection for DBI

--- R/db-impala.R
+++ R/db-impala.R
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# register virtual class
+#' @importFrom methods setOldClass
+setOldClass("impala_connection")
+
 #' Describe the Impala data source
 #'
 #' @name db_desc
